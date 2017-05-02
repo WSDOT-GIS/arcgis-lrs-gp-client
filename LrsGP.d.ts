fda9d07284d39c15e41fb3a8ec453f6340584926
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2017-05-02 12:27:32 -0700

    Updated type definitions

--- LrsGP.d.ts
+++ LrsGP.d.ts
@@ -1,4 +1,4 @@
-import { FeatureSet } from "arcgis-rest-api";
+import { Feature, FeatureSet, Point, Polyline } from "arcgis-rest-api";
 import { LrsGPParameters } from "./LrsGPParameters";
 
 export interface ILrsGPOptions {
@@ -6,6 +6,51 @@ export interface ILrsGPOptions {
     async?: boolean;
     pointTaskName?: string;
     linesTaskName?: string;
+}
+
+export interface IRoutePointFeatureAttributes {
+    OBJECTID: string;
+    RouteId: string;
+    Measure: number;
+    Distance: number;
+    INPUTOID: number;
+    LOC_ANGLE: number;
+}
+
+export interface IRouteSegmentFeatureAttributes {
+    OBJECTID: string;
+    RouteID: string;
+    Measure: string;
+    Distance: number;
+    INPUTOID: number;
+    PairID: number;
+    IsStart: number;
+    EndRouteId: number;
+    EndMeasure: number;
+    EndDistance: number;
+    End_InputOID: number;
+    PairID_1: number;
+    IsStart_1: number;
+    LOC_ERROR: string;
+    Shape_Length: number;
+}
+
+export interface IRoutePointFeature extends Feature {
+    attributes: IRoutePointFeatureAttributes;
+    geometry: Point;
+}
+
+export interface IRouteSegmentFeature extends Feature {
+    attributes: IRouteSegmentFeatureAttributes;
+    geometry: Polyline;
+}
+
+export interface IRoutePointFeatureSet extends FeatureSet {
+    features: IRoutePointFeature[];
+}
+
+export interface IRouteSegmentFeatureSet extends FeatureSet {
+    features: IRouteSegmentFeature[];
 }
 
 export class LrsGP {
@@ -19,11 +64,11 @@ export class LrsGP {
      * @param {module:LrsGPParameters} lrsGpParams - LRS GP Parameters.
      * @returns {Promise.<external:FeatureSet>} - Returns a promise with a Feature Set.
      */
-    public pointsToRouteEvents(lrsGpParams: LrsGPParameters): Promise<FeatureSet>;
+    public pointsToRouteEvents(lrsGpParams: LrsGPParameters): Promise<IRoutePointFeatureSet>;
     /**
      * Locates line segments along routes.
      * @param {module:LrsGPParameters} lrsGpParams - LRS GP Parameters.
      * @returns {Promise.<external:FeatureSet>} - Returns a promise with a Feature Set.
      */
-    public pointsToRouteSegments(lrsGpParams: LrsGPParameters): Promise<FeatureSet>;
+    public pointsToRouteSegments(lrsGpParams: LrsGPParameters): Promise<IRouteSegmentFeatureSet>;
 }
